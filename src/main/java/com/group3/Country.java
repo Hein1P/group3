85568c44eb1ff8f74048303a999f83a0df881f33
Hein Thu <40478652@live.napier.ac.uk>
2020-11-12 00:22:07 +0630

    Add Country class, world sql and chagnes in some files.

---
+++ src/main/java/com/group3/Country.java
@@ -0,0 +1,37 @@
+package com.group3;
+
+public class Country {
+    /**
+     * Country's Code
+     */
+    public String Code;
+
+    public String Name;
+
+    public String Continent;
+
+    public String Region;
+
+    public int SurfaceArea;
+
+    public int IndepYear;
+
+    public int Population;
+
+    public int LifeExpectancy;
+
+    public int GNP;
+
+    public int GNPOld;
+
+    public String LocalName;
+
+    public String GovernmentForm;
+
+    public String HeadofState;
+
+    public int Capital;
+
+    public String Code2;
+
+}
