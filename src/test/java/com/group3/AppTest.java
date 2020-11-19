59e1f9a890074cd1a920a0b2396a89ff8eb0b613
40478644 <40478644@live.napier.ac.uk>
2020-11-19 15:38:25 +0630

    Added testing for four tasks in App Test.java

--- src/test/java/com/group3/AppTest.java
+++ src/test/java/com/group3/AppTest.java
@@ -71,6 +71,110 @@ import static org.junit.jupiter.api.Assertions.*;
             cities.add(city);
             app.displaycitypopuinworld(cities);
         }
+        @Test
+        void displaycounpopuinasiaTestNull()
+        {
+            app.displaycounpopuinasia(null);
+        }
+        @Test
+        void displaycounpopuinasiaTestEmpty()
+        {
+            ArrayList<Country> countries = new ArrayList<Country>();
+            app.displaycounpopuinasia(countries);
+        }
+        @Test
+        void displaycounpopuinasiaTestEmptyCoun()
+        {
+            ArrayList<Country> countries = new ArrayList<Country>();
+            countries.add(null);
+            app.displaycounpopuinasia(countries);
+        }
+        @Test
+        void displaycounpopuinasiaTestNormal()
+        {
+            ArrayList<Country> countries = new ArrayList<Country>();
+            Country coun = new Country("MMR","Myanmar","Asia","Southeast Asia",45611000,2710);
+            countries.add(coun);
+            app.displaycounpopuinasia(countries);
+        }
+        @Test
+        void displaycounpopuincaribbeanTestNull()
+        {
+            app.displaycounpopuincaribbean(null);
+        }
+        @Test
+        void displaycounpopuincaribbeanTestEmpty()
+        {
+            ArrayList<Country> countries = new ArrayList<Country>();
+            app.displaycounpopuincaribbean(countries);
+        }
+        @Test
+        void displaycounpopuincaribbeanTestEmptyCoun()
+        {
+            ArrayList<Country> countries = new ArrayList<Country>();
+            countries.add(null);
+            app.displaycounpopuincaribbean(countries);
+        }
+        @Test
+        void displaycounpopuincaribbeanTestNormal()
+        {
+            ArrayList<Country> countries = new ArrayList<Country>();
+            Country coun = new Country("MMR","Myanmar","Asia","Southeast Asia",45611000,2710);
+            countries.add(coun);
+            app.displaycounpopuincaribbean(countries);
+        }
+        @Test
+        void displaytopcounpopuinworldTestNull()
+        {
+            app.displaytopcounpopuinworld(null);
+        }
+        @Test
+        void displaytopcounpopuinworldTestEmpty()
+        {
+            ArrayList<Country> countries = new ArrayList<Country>();
+            app.displaytopcounpopuinworld(countries);
+        }
+        @Test
+        void displaytopcounpopuinworldTestEmptyCoun()
+        {
+            ArrayList<Country> countries = new ArrayList<Country>();
+            countries.add(null);
+            app.displaytopcounpopuinworld(countries);
+        }
+        @Test
+        void displaytopcounpopuinworldTestNormal()
+        {
+            ArrayList<Country> countries = new ArrayList<Country>();
+            Country coun = new Country("MMR","Myanmar","Asia","Southeast Asia",45611000,2710);
+            countries.add(coun);
+            app.displaytopcounpopuinworld(countries);
+        }
+        @Test
+        void displaytopcounpopuinasiaTestNull()
+        {
+            app.displaytopcounpopuinasia(null);
+        }
+        @Test
+        void displaytopcounpopuinasiaTestEmpty()
+        {
+            ArrayList<Country> countries = new ArrayList<Country>();
+            app.displaytopcounpopuinasia(countries);
+        }
+        @Test
+        void displaytopcounpopuinasiaTestEmptyCoun()
+        {
+            ArrayList<Country> countries = new ArrayList<Country>();
+            countries.add(null);
+            app.displaytopcounpopuinasia(countries);
+        }
+        @Test
+        void displaytopcounpopuinasiaTestNormal()
+        {
+            ArrayList<Country> countries = new ArrayList<Country>();
+            Country coun = new Country("MMR","Myanmar","Asia","Southeast Asia",45611000,2710);
+            countries.add(coun);
+            app.displaytopcounpopuinasia(countries);
+        }
 
     }
 
