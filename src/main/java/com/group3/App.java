3363d303059d9e2584a707498a16670c02fd59a6
40478644 <40478644@live.napier.ac.uk>
2020-11-20 16:08:30 +0630

    Exchange Capital functions' names.

--- src/main/java/com/group3/App.java
+++ src/main/java/com/group3/App.java
@@ -68,7 +68,7 @@ public class App {
     /**
      * * Get the country name by population in descenting order in the world.
      */
-    public ArrayList<Country> getcountrypopuinworld() {
+    public ArrayList<Country> getCountryPopuinWorld() {
         try {
             // Create an SQL statement
             Statement stmt = con.createStatement();
@@ -92,7 +92,7 @@ public class App {
         }
     }
 
-    public void displaycounpopuinworld(ArrayList<Country> countries) {
+    public void displayCounPopuinWorld(ArrayList<Country> countries) {
         if(countries == null){
             System.out.println("There is no data in Arraylist of countries");
             return;
@@ -114,7 +114,7 @@ public class App {
     /**
      * * Get the country name by population in descenting order in asia.
      */
-    public ArrayList<Country> getcountrypopuinasia() {
+    public ArrayList<Country> getCountryPopuinAsia() {
         try {
             // Create an SQL statement
             Statement stmt = con.createStatement();
@@ -138,7 +138,7 @@ public class App {
         }
     }
 
-    public void displaycounpopuinasia(ArrayList<Country> countries) {
+    public void displayCounPopuinAsia(ArrayList<Country> countries) {
         if(countries == null){
             System.out.println("There is no data in Arraylist of countries");
             return;
@@ -161,7 +161,7 @@ public class App {
     /**
      * * Get the country name by population in descenting order in caribbean.
      */
-    public ArrayList<Country> getcountrypopuincaribbean() {
+    public ArrayList<Country> getCountryPopuinCaribbean() {
         try {
             // Create an SQL statement
             Statement stmt = con.createStatement();
@@ -184,7 +184,7 @@ public class App {
         }
     }
 
-    public void displaycounpopuincaribbean(ArrayList<Country> countries) {
+    public void displayCounPopuinCaribbean(ArrayList<Country> countries) {
         if(countries == null){
             System.out.println("There is no data in Arraylist of countries");
             return;
@@ -207,7 +207,7 @@ public class App {
     /**
      * * Get the top country name by population in descenting order in the world provided by user.
      */
-    public ArrayList<Country> gettopcountrypopuinworld(int n) {
+    public ArrayList<Country> getTopCountryPopuinWorld(int n) {
         try {
             // Create an SQL statement
             Statement stmt = con.createStatement();
@@ -231,7 +231,7 @@ public class App {
         }
     }
 
-    public void displaytopcounpopuinworld(ArrayList<Country> countries) {
+    public void displayTopCounPopuinWorld(ArrayList<Country> countries) {
         if(countries == null){
             System.out.println("There is no data in Arraylist of countries");
             return;
@@ -254,7 +254,7 @@ public class App {
     /**
      * * Get the top country name by population in descenting order in the asia provided by user.
      */
-    public ArrayList<Country> gettopcountriespopuinasia(int n) {
+    public ArrayList<Country> getTopCountriesPopuinAsia(int n) {
         try {
             // Create an SQL statement
             Statement stmt = con.createStatement();
@@ -278,7 +278,7 @@ public class App {
         }
     }
 
-    public void displaytopcounpopuinasia(ArrayList<Country> countries) {
+    public void displayTopCounPopuinAsia(ArrayList<Country> countries) {
         if(countries == null){
             System.out.println("There is no data in Arraylist of countries");
             return;
@@ -302,7 +302,7 @@ public class App {
     /**
      * * Get the top country name by population in descenting order in the region provided by user.
      */
-    public ArrayList<Country> gettopcountriespopuinsoutherneurope(int n) {
+    public ArrayList<Country> getTopCountriesPopuinSoutherneurope(int n) {
         try {
             // Create an SQL statement
             Statement stmt = con.createStatement();
@@ -326,7 +326,7 @@ public class App {
         }
     }
 
-    public void displaytopcounpopuinsoutherneurope(ArrayList<Country> countries) {
+    public void displayTopCounPopuinSoutherneurope(ArrayList<Country> countries) {
         if(countries == null){
             System.out.println("There is no data in Arraylist of countries");
             return;
@@ -349,7 +349,7 @@ public class App {
     /**
      * * Get the cities name by population in descenting order in world.
      */
-    public ArrayList<City> getcitypopuinworld() {
+    public ArrayList<City> getCityPopuinWorld() {
         try {
             // Create an SQL statement
             Statement stmt = con.createStatement();
@@ -374,7 +374,7 @@ public class App {
         }
     }
 
-    public void displaycitypopuinworld(ArrayList<City> cities) {
+    public void displayCityPopuinWorld(ArrayList<City> cities) {
         if(cities == null){
             System.out.println("There is no data in Arraylist of cities");
             return;
@@ -397,7 +397,7 @@ public class App {
     /**
      * * Get the cities name by population in descenting order in a Asia.
      */
-    public ArrayList<City> getcitypopuinasia() {
+    public ArrayList<City> getCityPopuinAsia() {
         try {
             // Create an SQL statement
             Statement stmt = con.createStatement();
@@ -422,7 +422,7 @@ public class App {
         }
     }
 
-    public void displaycitypopuinasia(ArrayList<City> cities) {
+    public void displayCityPopuinAsia(ArrayList<City> cities) {
         if(cities == null){
             System.out.println("There is no data in Arraylist of cities");
             return;
@@ -447,7 +447,7 @@ public class App {
     /**
      * * Get the cities name by population in descenting order in middle east.
      */
-    public ArrayList<City> getcitypopuinmiddleeast() {
+    public ArrayList<City> getCityPopuinMiddleEast() {
         try {
             // Create an SQL statement
             Statement stmt = con.createStatement();
@@ -472,7 +472,7 @@ public class App {
         }
     }
 
-    public void displaycitypopuinmiddleeast(ArrayList<City> cities) {
+    public void displayCityPopuinMiddleEast(ArrayList<City> cities) {
         if(cities == null){
             System.out.println("There is no data in Arraylist of cities");
             return;
@@ -494,7 +494,7 @@ public class App {
     /**
      * * Get the cities name by population in descenting order in Myanmar.
      */
-    public ArrayList<City> getcitypopuinMyanmar() {
+    public ArrayList<City> getCityPopuinMyanmar() {
         try {
             // Create an SQL statement
             Statement stmt = con.createStatement();
@@ -519,7 +519,7 @@ public class App {
         }
     }
 
-    public void displaycitypopuinMyanmar(ArrayList<City> cities) {
+    public void displayCityPopuinMyanmar(ArrayList<City> cities) {
         if(cities == null){
             System.out.println("There is no data in Arraylist of cities");
             return;
@@ -541,7 +541,7 @@ public class App {
     /**
      * * Get the cities name by population in descenting order in virginia.
      */
-    public ArrayList<City> getcitypopuinvirginia() {
+    public ArrayList<City> getCityPopuinVirginia() {
         try {
             // Create an SQL statement
             Statement stmt = con.createStatement();
@@ -566,7 +566,7 @@ public class App {
         }
     }
 
-    public void displaycitypopuinvirginia(ArrayList<City> cities) {
+    public void displayCityPopuinVirginia(ArrayList<City> cities) {
         if (cities == null)
         {
             System.out.println("There is no data in ArrayList of Cities");
@@ -589,7 +589,7 @@ public class App {
     /**
      * * Get the top City name by population in descenting order in the world provided by user.
      */
-    public ArrayList<City> gettopcitypopuinworld(int n) {
+    public ArrayList<City> getTopCityPopuinWorld(int n) {
         try {
             // Create an SQL statement
             Statement stmt = con.createStatement();
@@ -614,7 +614,7 @@ public class App {
         }
     }
 
-    public void displaytopcitypopuinworld(ArrayList<City> cities) {
+    public void displayTopCityPopuinWorld(ArrayList<City> cities) {
         // Check cities is not null
         if (cities == null)
         {
@@ -639,7 +639,7 @@ public class App {
     /**
      * * Get the top City name by population in descenting order in the asia provided by user.
      */
-    public ArrayList<City> gettopcitypopuinasia(int n) {
+    public ArrayList<City> getTopCityPopuinAsia(int n) {
         try {
             // Create an SQL statement
             Statement stmt = con.createStatement();
@@ -664,7 +664,7 @@ public class App {
         }
     }
 
-    public void displaytopcitypopuinasia(ArrayList<City> cities) {
+    public void displayTopCityPopuinAsia(ArrayList<City> cities) {
         // Check cities is not null
         if (cities == null)
         {
@@ -690,7 +690,7 @@ public class App {
     /**
      * * Get the top City name by population in descenting order in the caribbean provided by user.
      */
-    public ArrayList<City> gettopcitypopuincaribbean(int n) {
+    public ArrayList<City> getTopCityPopuinCaribbean(int n) {
         try {
             // Create an SQL statement
             Statement stmt = con.createStatement();
@@ -715,7 +715,7 @@ public class App {
         }
     }
 
-    public void displaytopcitypopuincaribbean(ArrayList<City> cities) {
+    public void displayTopCityPopuinCaribbean(ArrayList<City> cities) {
         // Check cities is not null
         if (cities == null)
         {
@@ -740,7 +740,7 @@ public class App {
     /**
      * * Get the top City name by population in descenting order in the myanmar provided by user.
      */
-    public ArrayList<City> gettopcitypopuinmyanmar(int n) {
+    public ArrayList<City> getTopCityPopuinMyanmar(int n) {
         try {
             // Create an SQL statement
             Statement stmt = con.createStatement();
@@ -765,7 +765,7 @@ public class App {
         }
     }
 
-    public void displaytopcitypopuinmyanmar(ArrayList<City> cities) {
+    public void displayTopCityPopuinMyanmar(ArrayList<City> cities) {
         // Check cities is not null
         if (cities == null)
         {
@@ -791,7 +791,7 @@ public class App {
     /**
      * * Get the top City name by population in descenting order in the virginia provided by user.
      */
-    public ArrayList<City> gettopcitypopuinvirginia(int n) {
+    public ArrayList<City> getTopCityPopuinVirginia(int n) {
         try {
             // Create an SQL statement
             Statement stmt = con.createStatement();
@@ -816,7 +816,7 @@ public class App {
         }
     }
 
-    public void displaytopcitypopuinvirginia(ArrayList<City> cities) {
+    public void displayTopCityPopuinVirginia(ArrayList<City> cities) {
         // Check cities is not null
         if (cities == null)
         {
@@ -853,86 +853,86 @@ public class App {
             a.connect(args[0]);
         }
 
-         // Get Country list in the world
-         ArrayList<Country> countries = a.getcountrypopuinworld();
-         // Display countries
-         a.displaycounpopuinworld(countries);
+        // Get Country list in the world
+        ArrayList<Country> countries = a.getCountryPopuinWorld();
+        // Display countries
+        a.displayCounPopuinWorld(countries);
 
-         // Get Country list in asia
-         ArrayList<Country> asiacountries = a.getcountrypopuinasia();
-         // Display countries
-         a.displaycounpopuinasia(asiacountries);
+        // Get Country list in asia
+        ArrayList<Country> asiacountries = a.getCountryPopuinAsia();
+        // Display countries
+        a.displayCounPopuinAsia(asiacountries);
 
-         // Get Country list in caribbeanm
-         ArrayList<Country> caribbeancountries = a.getcountrypopuincaribbean();
-         // Display countries
-         a.displaycounpopuincaribbean(caribbeancountries);
+        // Get Country list in caribbeanm
+        ArrayList<Country> caribbeancountries = a.getCountryPopuinCaribbean();
+        // Display countries
+        a.displayCounPopuinCaribbean(caribbeancountries);
 
-         // Get Top Country list in world by user input
-         ArrayList<Country> topcountriesinworld = a.gettopcountrypopuinworld(10);
-         // Display countries
-         a.displaytopcounpopuinworld(topcountriesinworld);
+        // Get Top Country list in world by user input
+        ArrayList<Country> topcountriesinworld = a.getTopCountryPopuinWorld(10);
+        // Display countries
+        a.displayTopCounPopuinWorld(topcountriesinworld);
 
-         //Get Top Country List in Asia by user input
-         ArrayList<Country> topcountriesinasia = a.gettopcountriespopuinasia(10);
-         //Display countries
-         a.displaytopcounpopuinasia(topcountriesinasia);
+        //Get Top Country List in Asia by user input
+        ArrayList<Country> topcountriesinasia = a.getTopCountriesPopuinAsia(10);
+        //Display countries
+        a.displayTopCounPopuinAsia(topcountriesinasia);
 
-         //Get Top Country List in Region by user input
-         ArrayList<Country> topcountriesinsoutherneurope = a.gettopcountriespopuinsoutherneurope(10);
-         //Display countries
-         a.displaytopcounpopuinsoutherneurope(topcountriesinsoutherneurope);
+        //Get Top Country List in Region by user input
+        ArrayList<Country> topcountriesinsoutherneurope = a.getTopCountriesPopuinSoutherneurope(10);
+        //Display countries
+        a.displayTopCounPopuinSoutherneurope(topcountriesinsoutherneurope);
 
-         // Get City list in the world
-         ArrayList<City> citypopuinworld = a.getcitypopuinworld();
-         //Display cities
-         a.displaycitypopuinworld(citypopuinworld);
+        // Get City list in the world
+        ArrayList<City> citypopuinworld = a.getCityPopuinWorld();
+        //Display cities
+        a.displayCityPopuinWorld(citypopuinworld);
 
-         // Get Country list in the asia
-         ArrayList<City> citypopuinasia = a.getcitypopuinasia();
-         //Display cities
-         a.displaycitypopuinasia(citypopuinasia);
+        // Get Country list in the asia
+        ArrayList<City> citypopuinasia = a.getCityPopuinAsia();
+        //Display cities
+        a.displayCityPopuinAsia(citypopuinasia);
 
-         // Get Country list in the middle east
-         ArrayList<City> citypopuinmiddleeast = a.getcitypopuinmiddleeast();
-         //Display cities
-         a.displaycitypopuinmiddleeast(citypopuinmiddleeast);
+        // Get Country list in the middle east
+        ArrayList<City> citypopuinmiddleeast = a.getCityPopuinMiddleEast();
+        //Display cities
+        a.displayCityPopuinMiddleEast(citypopuinmiddleeast);
 
-         // Get Country list in Myanmar
-         ArrayList<City> citypopuinMyanmar = a.getcitypopuinMyanmar();
-         //Display cities
-         a.displaycitypopuinMyanmar(citypopuinMyanmar);
+        // Get Country list in Myanmar
+        ArrayList<City> citypopuinMyanmar = a.getCityPopuinMyanmar();
+        //Display cities
+        a.displayCityPopuinMyanmar(citypopuinMyanmar);
 
         // Get Country list in Rangoon
-        ArrayList<City> citypopuinvirginia = a.getcitypopuinvirginia();
+        ArrayList<City> citypopuinvirginia = a.getCityPopuinVirginia();
         //Display cities
-        a.displaycitypopuinvirginia(citypopuinvirginia);
+        a.displayCityPopuinVirginia(citypopuinvirginia);
 
         // Get Top City list in world by user input
-        ArrayList<City> topcitiesinworld = a.gettopcitypopuinworld(10);
+        ArrayList<City> topcitiesinworld = a.getTopCityPopuinWorld(10);
         // Display Cities
-        a.displaytopcitypopuinworld(topcitiesinworld);
+        a.displayTopCityPopuinWorld(topcitiesinworld);
 
         //Get Top City list in asia by user input
-        ArrayList<City> topcitiesinasia = a.gettopcitypopuinasia(10);
+        ArrayList<City> topcitiesinasia = a.getTopCityPopuinAsia(10);
         // Display Cities
-        a.displaytopcitypopuinasia(topcitiesinasia);
+        a.displayTopCityPopuinAsia(topcitiesinasia);
 
 
         //Get Top City list in caribbean by user input
-        ArrayList<City> topcitiesincaribbean = a.gettopcitypopuincaribbean(10);
+        ArrayList<City> topcitiesincaribbean = a.getTopCityPopuinCaribbean(10);
         // Display Cities
-        a.displaytopcitypopuincaribbean(topcitiesincaribbean);
+        a.displayTopCityPopuinCaribbean(topcitiesincaribbean);
 
         // Get Top City list in myanmar by user input
-        ArrayList<City> topcitiesinmyanmar = a.gettopcitypopuinmyanmar(10);
+        ArrayList<City> topcitiesinmyanmar = a.getTopCityPopuinMyanmar(10);
         // Display Cities
-        a.displaytopcitypopuinmyanmar(topcitiesinmyanmar);
+        a.displayTopCityPopuinMyanmar(topcitiesinmyanmar);
 
         // Get Top City list in virginia by user input
-        ArrayList<City> topcitiesinvirginia = a.gettopcitypopuinvirginia(10);
+        ArrayList<City> topcitiesinvirginia = a.getTopCityPopuinVirginia(10);
         // Display Cities
-        a.displaytopcitypopuinvirginia(topcitiesinvirginia);
+        a.displayTopCityPopuinVirginia(topcitiesinvirginia);
 
 
         // Disconnect from database
